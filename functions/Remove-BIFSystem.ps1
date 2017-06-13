840895d240877edf5e2c617e002e0fddd2c0dc69
Andreas Östlund <andreasostlund@users.noreply.github.com>
2017-06-13 09:04:18 +0200

    Lagt skelett till Remove-BIFSystem

---
+++ functions/Remove-BIFSystem.ps1
@@ -0,0 +1,74 @@
+﻿<#
+	.SYNOPSIS        
+
+	.DESCRIPTION        
+
+    .PARAMETER         
+
+	.EXAMPLE        
+
+	.NOTES
+
+	.LINK
+
+#>
+Function Remove-BIFSystem {
+    [cmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$True)]
+        [string]$CustomerName,
+
+        [Parameter(Mandatory=$False)]
+        [string]$SystemName
+
+        <#
+        [Parameter(Mandatory=$True)]
+        [ValidateSet('Prod','Test','QA')]
+        [string]$Environment
+        #>        
+    )
+    DynamicParam {
+        $RuntimeParameterDictionary = _New-DynamicValidateSetParam -ParameterName "Environment" `
+                                                                   -ParameterType [DynParamQuotedString] `
+                                                                   -Mandatory $True `
+                                                                   -FillValuesWith "_OLL.BIF.Utils-dynamic-params_Get-EnvironmentShortNames" 
+
+        return $RuntimeParameterDictionary
+    }
+
+    BEGIN {
+		# If -debug is set, change $DebugPreference so that output is a little less annoying.
+		#	http://learn-powershell.net/2014/06/01/prevent-write-debug-from-bugging-you/
+		If ($PSBoundParameters['Debug']) {
+			$DebugPreference = 'Continue'
+		}
+
+        # Load parameter for Environment from dynamic param
+        $Environment = $PSBoundParameters["Environment"].OriginalString
+
+
+
+        if(-Not $script:EnvironmentConfig) {
+            Throw "Global Environment config is not set! Is the module properly loaded?"
+        }
+        
+        try {
+            $EnvConfigFile = $script:EnvironmentConfig[$Environment]
+            [xml]$ConfigData = Get-Content $EnvConfigFile -ErrorAction Stop
+        }
+        catch {
+            Throw "Could not load configuration from `"$EnvConfigFile`". Make sure the file exists and your account has access to it, or that EnvironmentConfig is defined, is the module loaded properly?"
+        }
+
+
+    }
+
+    PROCESS {
+
+        $System = Get-BIFSystem -CustomerName $CustomerName -SystemName $SystemName -Environment $Environment
+
+    }
+
+    END {
+    }
+}
