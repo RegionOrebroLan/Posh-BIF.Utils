6790db0a1a015e32f2288e96ca24cacf5fba4f7e
AndreasOstlund <AndreasOstlund@users.noreply.github.com>
2017-07-30 20:46:48 +0200

    misspelled function name

--- functions/Remove-BIFCareprovider.ps1
+++ functions/Remove-BIFCareprovider.ps1
@@ -24,7 +24,7 @@
     .LINK
 
 #>
-Function RemoveBIFCareprovider {
+Function Remove-BIFCareprovider {
     [cmdletBinding(SupportsShouldProcess=$True
                   ,ConfirmImpact='Medium'
                   ,DefaultParameterSetName='ByCareProviderHSAId')]
