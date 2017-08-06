5b9359d6aee4b23e571ce5318dee1c509ed2013d
AndreasOstlund <AndreasOstlund@users.noreply.github.com>
2017-08-06 18:04:53 +0200

    ConvertFrom-BIFConfigToString. error handling

--- public/ConvertFrom-BIFConfigToScript.ps1
+++ public/ConvertFrom-BIFConfigToScript.ps1
@@ -7,18 +7,20 @@
     .PARAMETER Environment
         Läser konfigurationen från angiven miljö.
 
+    .PARAMETER Path
+        Läser konfigurationen från angiven sökväg.
+        Parametern ApplyEnvironment måste anges för att sätta vad miljön skall kallas i den genererade konfigurationen.
+
+    .PARAMETER Data
+        Läser konfigurationen från angiven sträng.
+        Data till parametern kan även skickas in över pupeline.
+        Parametern ApplyEnvironment måste anges för att sätta vad miljön skall kallas i den genererade konfigurationen.
+
     .PARAMETER CustomerName
         Konverterar endast konfiguration för angiven kund.
 
     .PARAMETER ApplyEnvironment
         För de kommandon som skapas sätts denna miljö.
-
-    .PARAMETER Path
-        Läser konfigurationen från angiven sökväg.
-
-    .PARAMETER Data
-        Läser konfigurationen från angiven sträng.
-        Data till parametern kan även skickas in över pupeline.
 
     .EXAMPLE
         ConvertFrom-BIFConfigToScript -Environment Test
@@ -31,13 +33,13 @@
 
 #>
 Function ConvertFrom-BIFConfigToScript {
-    [cmdletBinding(DefaultParameterSetName='Data')]
+    [cmdletBinding(DefaultParameterSetName='Environment')]
     Param(
-        [Parameter(Mandatory=$False
+        [Parameter(Mandatory=$True
                   ,ParameterSetName='Path')]
         [string]$Path
 
-        ,[Parameter(Mandatory=$False
+        ,[Parameter(Mandatory=$True
                   ,ParameterSetName='Data'
                   ,ValueFromPipeline=$True)]
         [string]$Data
@@ -52,7 +54,7 @@ Function ConvertFrom-BIFConfigToScript {
     DynamicParam {
         $RuntimeParameterDictionary = _New-DynamicValidateSetParam -ParameterName "Environment" `
                                                                    -ParameterType [DynParamQuotedString] `
-                                                                   -Mandatory $False `
+                                                                   -Mandatory $True `
                                                                    -ExtraParameterProperties @{ParameterSetName='Environment'} `
                                                                    -FillValuesWith "_OLL.BIF.Utils-dynamic-params_Get-EnvironmentShortNames"
 
@@ -70,18 +72,31 @@ Function ConvertFrom-BIFConfigToScript {
 
         $Environment = $PSBoundParameters["Environment"].OriginalString
 
-        if($Environment) {
-            if(-Not $script:EnvironmentConfig) {
-                Throw "Global Environment config is not set! Is the module properly loaded? use Use-BIFSettings to re-read configuration data."
-            }
+        if(-Not $script:EnvironmentConfig) {
+            Throw "Global Environment config is not set! Is the module properly loaded? use Use-BIFSettings to re-read configuration data."
+        }
 
-            try {
+        try {
+
+            if($Environment) {
                 $EnvConfigFile = $script:EnvironmentConfig[$Environment]
-                [xml]$ConfigData = Get-Content $EnvConfigFile -ErrorAction Stop
+                [xml]$ConfigData = Get-Content $EnvConfigFile -ErrorAction Stop -Encoding UTF8
+            } elseif($Path) {
+                $EnvConfigFile = $Path
+                #TODO: This error should be handled some other way
+                if(-not $ApplyEnvironment) { Throw "Parameter ApplyEnvironment must be specified" }
+
+                [xml]$ConfigData = Get-Content $EnvConfigFile -ErrorAction Stop -Encoding UTF8
+            } elseif($Data) {
+                #TODO: This error should be handled some other way
+                if(-not $ApplyEnvironment) { Throw "Parameter ApplyEnvironment must be specified" }
+
+            } else {
+                # note supposed to happen because of parameter sets
             }
-            catch {
-                Throw "Could not load configuration from `"$EnvConfigFile`". Make sure the file exists and your account has access to it, or that EnvironmentConfig is defined, is the module loaded properly?"
-            }
+        }
+        catch {
+            Throw "Could not load configuration from `"$EnvConfigFile`". Make sure the file exists and your account has access to it, or that EnvironmentConfig is defined, is the module loaded properly?`r`n{0}" -f $_.Exception.Message
         }
 
         if($ApplyEnvironment) {
@@ -93,6 +108,7 @@ Function ConvertFrom-BIFConfigToScript {
 
         "Set-BIFEnvironment -Environment {0} -Version {1} -SystemAccessTemplate '{2}' -UserAccessTemplate '{3}'" -f $Environment, $ConfigData.OLLBIF.Environment.Version, $ConfigData.OLLBIF.Environment.SystemAccessTemplate, $ConfigData.OLLBIF.Environment.UserAccessTemplate
 
+        # loop all customers
         foreach($customer in $ConfigData.OLLBIF.Customers.Customer) {
 
             $CustomerString = 'Add-BIFCustomer -Environment {0} -CustomerName "{1}"' -f $Environment, $customer.Name
@@ -100,6 +116,7 @@ Function ConvertFrom-BIFConfigToScript {
                 $CustomerString += ' -ShortName "{0}"' -f $Customer.ShortName
             }
 
+            # loop any systems attached to customer
             foreach($system in $customer.Systems.System) {
 
                 $SystemString = 'Add-BIFSystem -Environment "{0}" -CustomerName "{1}" -SystemName "{2}" -SystemHSAId "{3}"' -f $Environment, $customer.name, $system.name, $system.hsaid
@@ -108,6 +125,7 @@ Function ConvertFrom-BIFConfigToScript {
 
                 #TODO: Handle exclude frm user ACL!
 
+                # loop any careproviders attached to system
                 foreach($careprovider in $system.Careproviders.careprovider) {
 
                     $CareproviderString = 'Add-BIFCareProvider -Environment "{0}" -CustomerName "{1}" -SystemHSAId "{2}" -CareproviderName "{3}" -CareProviderHSAId "{4}"' -f $Environment, $customer.name, $system.hsaid, $careprovider.name, $careprovider.hsaid
@@ -117,6 +135,7 @@ Function ConvertFrom-BIFConfigToScript {
 
             }
 
+            # loop any careproviders for customer
             foreach($careprovider in $customer.Careproviders.careprovider) {
                 #TODO: Handle exclude frm user ACL!
 
